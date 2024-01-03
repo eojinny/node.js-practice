0c464d11e4b4be29686d226d4ef863fcd3566656
eojinny <96863137+eojinny@users.noreply.github.com>
2024-01-03 10:06:19 +0900

    Delete src directory

--- src/app/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { Test, Exception } from '../controllers/controller.js';
-
-export const Router = express.Router();
-
-Router.get('/test', Test);
-
-Router.get('/exception/:flag', Exception);
